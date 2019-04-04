fce2fdf877cae9e325362d0933d9fa1f2a82a824
Markus Tarn <Markus.Tarn@myjar.com>
2019-04-04 23:02:58 +0300

    Initial front-end

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const config = require('config');
+const es6Renderer = require('express-es6-template-engine');
+const join = require('path').join;
 const app = express();
+const logResponse = (req, res, next) => { console.log(req.query); next(); };
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.engine('html', es6Renderer);
+app.set('views', join(__dirname, 'views'));
+app.set('view engine', 'html');
+
+app.get('/respond', logResponse, (req, res) => res.json(req.query));
 app.get('/random', (req, res) => res.json({ response: (Math.floor(Math.random() * Math.floor(2))) }));
 app.get('/randomBoolean', (req, res) => res.json({ response: (Math.floor(Math.random() * Math.floor(2)) ? true : false) }));
-app.get('/:param', (req, res) => res.json({ response: req.params.param }));
+app.get('/respond/:param', logResponse, (req, res) => res.json({ response: req.params.param }));
+app.get('/', logResponse, (req, res) => res.render('index'));
 
 const startMsg = `${process.env.npm_package_name} service started on port ${config.get('service.port') || 3000}.`;
 app.listen(config.get('service.port' || 3000), () => console.log('info', startMsg));
