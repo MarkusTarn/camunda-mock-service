e28669819b61d4fa993ea49bd50abc580f96fe26
Markus Tarn <Markus.Tarn@myjar.com>
2019-04-08 16:21:05 +0300

    Disable file upload without button

--- public/index.js
+++ public/index.js
@@ -1,4 +1,4 @@
-const predicate = (a, b) => b.name < a.name ? 1 : b.name < a.name ? -1 : b.version > a.version ? 1 : -1;
+const predicate = (a, b) => (b.name < a.name ? 1 : (b.name < a.name ? -1 : (b.version > a.version ? 1 : -1)));
 
 function initialize() {
     const elements = document.querySelectorAll('.collapsible');
@@ -11,14 +11,27 @@ async function alert(message) {
     M.toast({ html: message, classes: 'red lighten-2', displayLength: 10000 });
 }
 
+async function copyToClipboard(id) {
+    navigator.clipboard.writeText(id).then(M.toast({ html: 'Copied to Clipboard!' }));
+}
+
+async function enableFileUpload(id) {
+    document.getElementById(id + '-update').classList.remove('disabled');
+}
+
 async function loadProcesses() {
     const processes = await getProcessesAsync();
-    await asyncForEach(processes, addInstanceCountAsync);
-    processes.sort(predicate).forEach(addProcess);
+
+    if (processes.length < 1)  {
+        alert('No processes found')
+    } else {
+        await asyncForEach(processes, addInstanceCountAsync);
+        processes.sort(predicate).forEach(addProcess);
+    }
 }
 
 async function getProcessesAsync() {
-    let processes = await fetch('http://localhost:8080/engine-rest/process-definition')
+    const processes = await fetch('http://localhost:8080/engine-rest/process-definition')
         .then(response => response.json())
         .catch(error => alert(error));
     return processes;
@@ -34,34 +47,32 @@ async function startProcessAsync(id) {
         }
     }
 
-    fetch('http://localhost:8080/engine-rest/process-definition/' + id + '/start', {
+    return await fetch('http://localhost:8080/engine-rest/process-definition/' + id + '/start', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(payload)
     })
         .then(response => response.json())
-        .then(() => {
-            document.getElementById(id + '-count').innerHTML = parseInt(document.getElementById(id + '-count').innerHTML) + 1;
-            M.toast({ html: 'Process started' });
-        })
+        .then(document.getElementById(id + '-count').innerHTML = parseInt(document.getElementById(id + '-count').innerHTML) + 1)
+        .then(M.toast({ html: 'Process started' }))
         .catch(error => alert(error));
 }
 
 async function deleteProcessAsync(id) {
     fetch('http://localhost:8080/engine-rest/process-definition/' + id + '?cascade=true&skipCustomListeners=true&skipIoMappings=true', { method: 'DELETE' })
-        .then(() => {
-            prosess.parentNode.removeChild(document.getElementById(id));
-            M.toast({ html: 'Process definition deleted' });
-        })
+        .then(document.getElementById(id).parentNode.removeChild(document.getElementById(id)))
+        .then(M.toast({ html: 'Process definition deleted' }))
         .catch(error => alert(error));
 }
 
 async function addInstanceCountAsync(process) {
-    const data = await fetch('http://localhost:8080/engine-rest/process-instance/count?processDefinitionId=' + process.id)
+    return await fetch('http://localhost:8080/engine-rest/process-instance/count?processDefinitionId=' + process.id)
         .then(response => response.json())
+        .then(data => {
+            process.count = data.count;
+            return process;
+        })
         .catch(error => alert(error));
-    process.count = data.count;
-    return process;
 }
 
 async function asyncForEach(array, callback) {
@@ -97,10 +108,6 @@ async function updateProcessAsync(id) {
     request.send(data);
 }
 
-async function copyToClipboard(id) {
-    navigator.clipboard.writeText(id).then(() => M.toast({ html: 'Copied to Clipboard!' }));
-}
-
 async function addProcess(process) {
     document.getElementById('processList').innerHTML += `
     <li id="${process.id}" data-name="${process.name}" data-tenant="${process.tenantId}">
@@ -122,7 +129,7 @@ async function addProcess(process) {
                 <div class="col s6">
                     <div class="file-field input-field">
                         <div class="btn">
-                            <span>Model</span><input id="${process.id + '-file'}" type="file">
+                            <span>Model</span><input id="${process.id + '-file'}" type="file" onchange="enableFileUpload('${process.id}');">
                         </div>
                         <div class="file-path-wrapper">
                             <input class="file-path validate" type="text">
@@ -131,7 +138,7 @@ async function addProcess(process) {
                 </div>
                 <div class="col s6">
                     <button class="btn waves-effect waves-light red lighten-2 right" onclick="deleteProcessAsync('${process.id}');">Delete<i class="material-icons right">delete</i></button>
-                    <button class="btn waves-effect waves-light right" onclick="updateProcessAsync('${process.id}');">Update<i class="material-icons right">build</i></button>
+                    <button class="btn waves-effect waves-light right disabled" id="${process.id + '-update'}" onclick="updateProcessAsync('${process.id}');">Update<i class="material-icons right">build</i></button>
                 </div>
             </div>
         </div>
